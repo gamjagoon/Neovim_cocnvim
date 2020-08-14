81fabdf49d1ec2214596ffefedfa634d97aceeb7
Kim_Min_Jae <51118024+gamjagoon@users.noreply.github.com>
2020-08-14 09:07:43 +0900

    Update zshpluginstall.sh

--- zshpluginstall.sh
+++ zshpluginstall.sh
@@ -6,7 +6,8 @@ chsh -s $(which zsh)
 git clone https://github.com/nvm-sh/nvm.git .nvm
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>! ~/.zshrc
 cd ~/.local 
 wget https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage
 chmod u+x nvim.appimage
