4dd217d24b97b3ca2c48b5af6a1406720f1f88e4
Minjae Kim <minjae@islab.re.kr>
2020-12-18 22:33:47 +0900

    fix

--- setup.sh
+++ setup.sh
@@ -6,16 +6,16 @@ fi
 
 if [ -d "$HOME/.config/coc" ]; then
 	if [ -d "$HOME/.config/coc/ultisnips" ]; then
-		cp .config/coc/ultisnips/* $HOME/.config/coc/ultisnips/
+		cp config/coc/ultisnips/* $HOME/.config/coc/ultisnips/
 	else 
-		mv .config/coc/ultisnips $HOME/.config/coc/
+		mv config/coc/ultisnips $HOME/.config/coc/
 	fi
 	else
-	mv .config/coc $HOME/.config/
+	mv config/coc $HOME/.config/
 fi
 
 if [ -d "$HOME/.config/nvim" ];then
-	cp .config/nvim/* $HOME/.config/nvim/
+	cp config/nvim/* $HOME/.config/nvim/
 else
-	mv .config/nvim $HOME/.config/
+	mv config/nvim $HOME/.config/
 fi
