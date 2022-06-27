5ee70af7e4ec0398bd81556adaa93796f0fa3885
Min Jae Kim <rlaalswo201@gmail.com>
2022-06-27 17:53:29 +0900

    [Update]LSP setup

--- lua/user/lsp/handlers.lua
+++ lua/user/lsp/handlers.lua
@@ -9,6 +9,7 @@ M.setup = function()
     { name = "DiagnosticSignInfo", text = "" },
   }
 
+
   for _, sign in ipairs(signs) do
     vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
   end
